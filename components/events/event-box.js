1fde239a15a3b439d59baecee93352ed49e2ba9d
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-01-06 10:40:32 +0330

    added event links to the slider

--- components/events/event-box.js
+++ components/events/event-box.js
@@ -3,21 +3,30 @@ import Image from "next/image";
 
 import getDate from "@/lib/getDate";
 import classes from "./event-box.module.css";
+import Link from "next/link";
 
-export default function EventBox({ eventData, className }) {
-  const { images, name, date, targetURL } = eventData;
+export default function EventBox({ eventData }) {
+  const { images, name, date, url } = eventData;
 
   const formatedDate = getDate(date);
 
   return (
     <div className={classes.event}>
-      <Image
-        className={`${classes["event-image"]}`}
-        src={images[0]}
-        alt={`${name} at Kemistry Night Club`}
-        width={300}
-        height={300}
-      />
+      <div className={classes["event-image-box"]}>
+        <Image
+          className={`${classes["event-image"]}`}
+          src={images[0]}
+          alt={`${name} at Kemistry Night Club`}
+          width={300}
+          height={300}
+        />
+        <div className={classes["event-action"]}>
+          <Link href="/reserve-table">Reserve a Table</Link>
+          <Link target="_blank" rel="nofollow" href={url}>
+            Buy Tickets
+          </Link>
+        </div>
+      </div>
       <h2 className={classes["event-name"]}>{name}</h2>
       <p className={classes["event-date"]}>{formatedDate}</p>
     </div>
