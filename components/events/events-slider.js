87081213c77b7a48c56136bc26cced240ea3f28c
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-01-14 09:30:55 +0330

    fixed homepage slider

--- components/events/events-slider.js
+++ components/events/events-slider.js
@@ -2,7 +2,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { eventsSettings as settings } from "@/lib/carouselSettings";
+import { sliderSettings as settings } from "@/lib/carouselSettings";
 
 import classes from "./events-slider.module.css";
 import EventBox from "./event-box";
