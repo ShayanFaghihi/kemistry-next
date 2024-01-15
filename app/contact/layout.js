2e9b5253adcc82a6d6e544e570c0900af632d30c
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-01-15 09:21:35 +0330

    added contact us page layout

---
+++ app/contact/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Contact - Kemistry Night Club",
+  description:
+    "Have any questions? Please contact Kemistry Night Club fort lauderdale nightclub.",
+};
+
+export default function ContactPageLayout({ children }) {
+  return children;
+}
