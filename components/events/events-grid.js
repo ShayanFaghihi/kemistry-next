a972a5ab8457b9593acda89498057c8c84f3ec92
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-06-12 21:04:40 +0330

    Reverted events to Dice script

--- components/events/events-grid.js
+++ components/events/events-grid.js
@@ -1,64 +1,65 @@
-// "use client";
+"use client";
 import { Suspense } from "react";
 import { getEvents } from "@/lib/events";
 
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import LoadingIndicator from "../UI/loading-indicator";
 import EventBox from "./event-box";
 import EventGridBox from "./event-grid-box";
 
 import classes from "./events-grid.module.css";
 
-export default async function EventsGrid() {
-  const events = await getEvents();
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-  //   script.src = "https://widgets.dice.fm/dice-event-list-widget.js";
-  //   script.async = true;
+export default function EventsGrid() {
+  // const events = await getEvents();
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://widgets.dice.fm/dice-event-list-widget.js";
+    script.async = true;
 
-  //   script.onload = () => {
-  //     // Once the external script is loaded, create DiceEventListWidget
-  //     DiceEventListWidget.create({
-  //       information: "simple",
-  //       eventTitle: "event",
-  //       showImages: true,
-  //       showAudio: true,
-  //       showNewBadge: false,
-  //       hidePostponed: false,
-  //       hideCancelled: false,
-  //       layout: "gallery",
-  //       roundButtons: false,
-  //       theme: "dark",
-  //       fontFamily: "inherit",
-  //       partnerId: "c8ddfba8",
-  //       apiKey: "HEpHpEIoQc3ssGz6UFT8O2vgZSsATa2L14lfW5j6",
-  //       version: 2,
-  //       showPrice: true,
-  //       __p: true,
-  //       __pc: { brandColor: "black", rounded: false },
-  //       title: false,
-  //       highlightColour: "#4d9308",
-  //       promoters: ["307 SW 2nd St LLC"],
-  //     });
-  //   };
+    script.onload = () => {
+      // Once the external script is loaded, create DiceEventListWidget
+      DiceEventListWidget.create({
+        information: "simple",
+        eventTitle: "event",
+        showImages: true,
+        showAudio: true,
+        showNewBadge: false,
+        hidePostponed: false,
+        hideCancelled: false,
+        layout: "gallery",
+        roundButtons: false,
+        theme: "dark",
+        fontFamily: "inherit",
+        partnerId: "c8ddfba8",
+        apiKey: "HEpHpEIoQc3ssGz6UFT8O2vgZSsATa2L14lfW5j6",
+        version: 2,
+        showPrice: true,
+        __p: true,
+        __pc: { brandColor: "black", rounded: false },
+        title: false,
+        highlightColour: "#4d9308",
+        promoters: ["307 SW 2nd St LLC"],
+      });
+    };
 
-  //   document.getElementById("dice-event-list-widget").appendChild(script);
+    document.getElementById("dice-event-list-widget").appendChild(script);
 
-  //   return () => {
-  //     // Clean up if needed
-  //     script.onload = null;
-  //   };
-  // }, []);
+    return () => {
+      // Clean up if needed
+      script.onload = null;
+    };
+  }, []);
 
   return (
     <>
       <h1 className={classes.heading}>Kemistry Upcoming Events</h1>
       <div id="dice-event-list-widget" className={classes["events-widget"]}>
-        <Suspense fallback={<LoadingIndicator />}>
-          {events.map((event) => (
+        <LoadingIndicator />
+        {/* <Suspense fallback={<LoadingIndicator />}> */}
+        {/* {events.map((event) => (
             <EventGridBox key={event.id} eventData={event} />
-          ))}
-        </Suspense>
+          ))} */}
+        {/* </Suspense> */}
       </div>
       <div>
         <EventBox
