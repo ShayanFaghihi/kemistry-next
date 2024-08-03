4d0dc89788b94b8afc203d94441626fef8048d19
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-08-03 18:53:53 +0330

    added resend add contact to audience from footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,3 +1,7 @@
+"use client";
+import { useState } from "react";
+import { Resend } from "resend";
+
 import Image from "next/image";
 import Container from "../UI/container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,6 +18,18 @@ import logoWhite from "@/assets/whiteLogo.png";
 import beatboxLogo from "@/assets/BeatBoxLogo2024.png";
 
 export default function Footer() {
+  const [userEmail, setUserEmail] = useState("");
+  const resend = new Resend("re_123456789");
+
+  const addContactHandler = (e) => {
+    e.preventDefault();
+    resend.contacts.create({
+      email: userEmail,
+      unsubscribed: false,
+      audienceId: "7d0ff667-7ea2-4266-a355-6374279c01eb",
+    });
+  };
+
   return (
     <footer className={classes["footer-section"]}>
       <Container className={classes.container}>
@@ -23,12 +39,13 @@ export default function Footer() {
         <div className={classes["footer-contact"]}>
           <h2>Stay Connected</h2>
           <p>Get all the latest content delivered right to your inbox</p>
-          <form>
+          <form onSubmit={addContactHandler}>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="Enter a valid email address"
+              onChange={(e) => setUserEmail(e.target.value)}
               required
             />
             <button type="submit">Sign Up</button>
