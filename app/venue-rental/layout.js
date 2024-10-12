9249f3e37128cac08eef7dac7c0141a1ce045025
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-10-12 23:12:39 +0330

    edited venute meta desc

--- app/venue-rental/layout.js
+++ app/venue-rental/layout.js
@@ -1,7 +1,7 @@
 export const metadata = {
   title: "Venue Rental & Event Services - Kemistry Nightclub",
   description:
-    "Kemisty offers event planning services, from catering and bar coordination to booking entertainment and designing layouts, helping clients bring their vision to life.",
+    "Kemistry offers event planning services, from catering and bar coordination to booking entertainment and designing layouts, helping clients bring their vision to life.",
   alternates: {
     canonical: "/venue-rental",
   },
